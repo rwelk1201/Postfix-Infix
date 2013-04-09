2a0aa9b4d503230cf6035cdae04376be42f14588
Ryan Welker <r.welker1201@gmail.com>
2013-04-08 23:29:24 -0500

    ADTList now from-zero

--- src/project_385/ADTList.java
+++ src/project_385/ADTList.java
@@ -57,35 +57,41 @@ public class ADTList<T>{
 	public int size() {	//returns numItems
 		return numItems;
 	}
-
-	public void add(int position, Object item) {	//inserts [item] at index [position]
-		if(isFull()||position>size()+1||position<1){	//return false if list is full or index is out of range
-			curSize++;
-			T[] tmp=(T[])new Object[curSize+1];
-			for(int i=0;i<curSize;i++){
-				tmp[i]=array[i];
-			}
+	
+	public int ensureSize(int length){
+		if(length>curSize){
+			T[] tmp=(T[])new Object[length];
+			System.arraycopy(array, 0, tmp, 0, array.length);
 			array=tmp;
+			curSize=length;
+		}
+		return(curSize);
+	}
+
+	public boolean add(int position, Object item) {	//inserts [item] at index [position]
+		if(isFull()){
+			ensureSize(curSize+1);
+		}
+		if(position>size()-1||position<0){	//return false if list is full or index is out of range
+			return(false);
 		}else{
 			for(int i=size()+1;i>position;i--){		//loops through array in reverse, shifting each element to the right one position
 				array[i]=array[i-1];				//down to the provided index
-			}				//sets the new array value and increments numItems
+			}
+			array[position]=(T)item;
+			numItems++;								//sets the new array value and increments numItems
+			return(true);
 		}
-		array[position]=(T)item;
-		numItems++;
+		
 	}
 
-	public void add(Object item) {				//inserts object [item] at end of list
+	public boolean add(Object item) {				//inserts object [item] at end of list
 		if(isFull()){
-			curSize++;
-			T[] tmp=(T[])new Object[curSize+1];
-			for(int i=0;i<curSize;i++){
-				tmp[i]=array[i];
-			}
-			array=tmp;
+			ensureSize(curSize+1);
 		}
 		array[size()+1]=(T)item;
 		numItems++;
+		return(true);
 	}
 
 	public boolean replace(int position, Object item) {	//replaces object at index [position] with object [item]
